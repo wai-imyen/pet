f5690d69e4dd0a93339a5c08830cfe0ee9ad7477
Yen <wai.imyen@gmail.com>
2021-10-07 16:09:40 +0800

    update: set auth to pet

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -9,6 +9,11 @@ use Illuminate\Validation\Rule;
 
 class PetController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
